39e3458b529903f03e8911913573720b7a90f5c6
franks42 <frank.siebenlist@gmail.com>
2013-03-15 20:50:09 -0700

    1.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.franks42/cljs-uuid-utils "0.1.3"
+(defproject org.clojars.franks42/cljs-uuid-utils "1.0.0-SNAPSHOT"
   :description "ClojureScript micro-library with an implementation of a type 4, random UUID generator compatible with RFC-4122 and cljs.core/UUID (make-random-uuid), a uuid-string conformance validating predicate (valid-uuid?), and a UUID factory from uuid-string with conformance validation (make-uuid-from)."
-  :url "https://github.com/franks42/cljs-random-uuid"
+  :url "https://github.com/franks42/cljs-uuid-utils"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
